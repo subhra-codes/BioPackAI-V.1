68c54f45cb690d806df26f5d7311f464365f1d4e
Subhrajit Biswas <subhra.codes@gmail.com>
2026-09-17 11:32:09 +0530

    Fix GitHub Pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/BioPackAI/',
+  base: '/BioPackAI-V.1/',
   plugins: [react()],
 })
